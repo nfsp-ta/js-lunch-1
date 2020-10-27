8af91c0b32bdf30269b390d7369692400e16a1a8
fairclotha <fairclotha@infinity-software.com>
2020-10-27 18:29:53 -0400

    styles

--- lib.js
+++ lib.js
@@ -2,7 +2,7 @@ export function out(t) {
   const txt = typeof t === "string" ? t : JSON.stringify(t, null, 2);
   const txtNode = document.createTextNode(txt);
   const pre = document.createElement("pre");
-  pre.className = "alert alert-secondary m-3";
+  pre.className = "alert";
   pre.appendChild(txtNode);
   document.getElementById("root").appendChild(pre);
 }
